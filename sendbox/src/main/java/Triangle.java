db33d188dee9f254c34d7a5c01a6d8f51e50e921
Alexey <leha-5@mail.ru>
2024-11-20 22:46:03 +0400

    Создан класс Triangle для представления треугольников. Реализованы методы вычисления периметра и площади

--- sendbox/src/main/java/Triangle.java
+++ sendbox/src/main/java/Triangle.java
@@ -1,19 +1,25 @@
 public class Triangle {
     public static void main(String[] args) {
-        var side1 = 1;
-        var side2 = 2;
-        var side3 = 3;
+        printPerimeterTriangle(1,2,3);
+        printSquareTriangle(4.0,5.0,6.0);
+    }
 
-        System.out.println("Периметр треугольника = " + (side1 + side2 + side3));
+    private static double printSquareTriangle(double a, double b, double c) {
+        System.out.println("Площадь треугольника со сторонами " + a + " и " + b + " и " + c + " = " + squareAre(a,b,c));
+        return a;
+    }
 
-        System.out.println("Площадь треугольника = ");
+    private static double squareAre(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
 
-        /*
-        Площадь треугольника может быть найдена по формуле Герона: S = √ (p * (p - a) * (p - b) * (p - c)),
-        где a, b и c - длины сторон треугольника;
-        p - полупериметр треугольника: p = (a + b + c) / 2.
-        Найдём величину полупериметра заданного треугольника: p = (11 + 13 + 20) / 2 = 44/2 = 22.
-        Тогда площадь треугольника равна. S = √ (22 * (22 - 11) * (22 - 13) * (22 - 20)) = √ (22 * 11 * 9 * 2) = √4356 = 66. Ответ: S = 66.
-         */
+    static void printPerimeterTriangle(double side1, double side2, double side3){
+        System.out.println("Периметр треугольника по трём сторонам = " + perimeterArea(side1, side2, side3));
+    }
+
+    private static double perimeterArea(double a, double b, double c) {
+        return a + b + c;
     }
 }
+
