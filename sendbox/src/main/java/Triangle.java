fc943581b9ed753e8823fed25f64b3c021c97590
Alexey <leha-5@mail.ru>
2024-11-23 17:36:20 +0400

    Исправил замечания

--- sendbox/src/main/java/Triangle.java
+++ sendbox/src/main/java/Triangle.java
@@ -1,25 +1,26 @@
 public class Triangle {
+    static double side1 = 2;
+    static double side2 = 2;
+    static double side3 = 2;
+
+    public Triangle(double side1, double side2, double side3) {
+        Triangle.side1 = side1;
+        Triangle.side2 = side2;
+        Triangle.side3 = side3;
+    }
+
+    public static double getArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3)); // Calculate the area using Heron's formula
+    }
+
+    public static double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
     public static void main(String[] args) {
-        printPerimeterTriangle(1,2,3);
-        printSquareTriangle(4.0,5.0,6.0);
-    }
-
-    private static double printSquareTriangle(double a, double b, double c) {
-        System.out.println("Площадь треугольника со сторонами " + a + " и " + b + " и " + c + " = " + squareAre(a,b,c));
-        return a;
-    }
-
-    private static double squareAre(double a, double b, double c) {
-        double p = (a + b + c) / 2;
-        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
-    }
-
-    static void printPerimeterTriangle(double side1, double side2, double side3){
-        System.out.println("Периметр треугольника по трём сторонам = " + perimeterArea(side1, side2, side3));
-    }
-
-    private static double perimeterArea(double a, double b, double c) {
-        return a + b + c;
+        System.out.println("Площадь треугольника = " + Triangle.getArea());
+        System.out.println("Периметр треугольника по трём сторонам = " + Triangle.getPerimeter());
     }
 }
 
